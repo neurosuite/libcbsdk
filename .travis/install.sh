929fa1433f108fa29a91293f33b031ef03028255
Florian Franzen <Florian.Franzen@gmail.com>
2018-01-21 17:34:47 +0100

    CI: Update travis config

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    if [ "$QT_VERSION" = "qt5" ]; then
-        sudo add-apt-repository -y ppa:beineri/opt-qt55
-    fi
-    sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
-    sudo apt-get update
-    sudo apt-get install -y cmake cmake-data
-
-    if [ "$QT_VERSION" = "qt4" ]; then
-        sudo apt-get install -y libqt4-xml
-    fi
-    if [ "$QT_VERSION" = "qt5" ]; then
-        sudo apt-get install -y qt55base
-        source /opt/qt55/bin/qt55-env.sh
-    fi
-fi
-
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     brew update
     brew install $QT_VERSION
